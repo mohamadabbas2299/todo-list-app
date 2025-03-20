cef16ef78bcd36fadfdc8916304636a3a13069f4
mohamadabbas2299 <mohamadabbas2299@gmail.com>
2025-03-20 10:39:31 +0530

    Initial commit

---
+++ src/test/java/com/abbas/todo_list_app/TodoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbas.todo_list_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
